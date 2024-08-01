8e2affa0760e539d52a8dd5515759ceb47a17b9a
Martin Starman <martin@goodsailors.com>
2024-08-01 20:08:11 +0200

    Set active layer

--- src/gui/right_sidebar.rs
+++ src/gui/right_sidebar.rs
@@ -4,12 +4,16 @@ use bevy_egui::{
     EguiContexts,
 };
 
-use crate::{component::layer::Layer, event::new_layer::NewLayer};
+use crate::{
+    component::layer::Layer,
+    event::{new_layer::NewLayer, set_active_layer::SetActiveLayer},
+};
 
 pub fn right_sidebar(
     mut egui_contexts: EguiContexts,
     mut layers_q: Query<&mut Layer>,
-    mut event_writer: EventWriter<NewLayer>,
+    mut new_layer_event: EventWriter<NewLayer>,
+    mut set_active_layer_event: EventWriter<SetActiveLayer>,
 ) {
     egui::SidePanel::right("right_sidebar")
         .resizable(false)
@@ -17,13 +21,17 @@ pub fn right_sidebar(
         .max_width(100.) // TODO
         .show(egui_contexts.ctx_mut(), |ui| {
             if ui.button("new layer").clicked() {
-                event_writer.send(NewLayer {});
+                new_layer_event.send(NewLayer {});
             }
 
             egui::ScrollArea::vertical()
                 .auto_shrink(false)
                 .show(ui, |ui| {
                     for mut layer in &mut layers_q {
+                        if ui.add(egui::RadioButton::new(layer.active, "")).clicked() {
+                            set_active_layer_event.send(SetActiveLayer { index: layer.index });
+                        }
+
                         ui.text_edit_singleline(&mut layer.name);
                     }
                 })
